ceb5b0e62a644c06baaccb145aa010155a3eeb3e
Hacker-JeT <107682079+Hacker-JeT@users.noreply.github.com>
2022-06-20 12:06:15 +0530

    Add files via upload

---
+++ converter.js
@@ -0,0 +1,3 @@
+function LengthConverter(valNum) {
+  document.getElementById("outputFeet").innerHTML=valNum*3.2808;
+}
--- index.js
+++ index.js
@@ -1,27 +1,11 @@
-let screen = document.getElementById('screen');
-buttons = document.querySelectorAll('button');
-let screenValue = '';
-for (item of buttons) {
-    item.addEventListener('click', (e) => {
-        buttonText = e.target.innerText;
-        console.log('Button text is ', buttonText);
-        if (buttonText == 'X') {
-            buttonText = '*';
-            screenValue += buttonText;
-            screen.value = screenValue;
-        }
-        else if (buttonText == 'C') {
-            screenValue = "";
-            screen.value = screenValue;
-        }
-        else if (buttonText == '=') {
-            screen.value = eval(screenValue);
-        }
-        else {
-            screenValue += buttonText;
-            screen.value = screenValue;
-        }
-
-    })
+function multi(){
+    window.location="table.html";
 }
 
+function cal(){
+    window.location="calculator.html";
+}
+
+function con(){
+    window.location="converter.html";
+}
